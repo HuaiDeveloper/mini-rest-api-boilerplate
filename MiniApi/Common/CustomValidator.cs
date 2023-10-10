e8c62d3088f0ac47ffd4846048978fe871764222
Sam <huaideveloper@gmail.com>
2023-10-10 11:37:53 +0800

    Add "CustomValidator"

---
+++ MiniApi/Common/CustomValidator.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniApi.Common;
+
+public class CustomValidator
+{
+    public static bool TryValidateObject<T>(T instance, out ICollection<ValidationResult> validationResults)
+    {
+        validationResults = new List<ValidationResult>();
+        
+        if (instance == null) return false;
+
+        return Validator.TryValidateObject(
+            instance,
+            new ValidationContext(instance),
+            validationResults,
+            true);
+    }
+}
