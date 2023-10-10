43f174ace56b87077bab6977bbbe99f1110351eb
Sam <huaideveloper@gmail.com>
2023-10-10 15:22:21 +0800

    Add "Serilog"

--- MiniApi/Program.cs
+++ MiniApi/Program.cs
@@ -3,35 +3,61 @@ using System.Text.Json.Serialization;
 using MiniApi.Application;
 using MiniApi.Middleware;
 using MiniApi.Persistence;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddHttpContextAccessor();
-
-builder.Services.AddPersistence();
-builder.Services.AddMiddleware();
-builder.Services.AddApplication();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+        .WriteTo.Console());
+
+    builder.Services.AddHttpContextAccessor();
+
+    builder.Services.AddPersistence();
+    builder.Services.AddMiddleware();
+    builder.Services.AddApplication();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseSerilogRequestLogging();
+
+    app.UseApplication();
+    app.UseCustomMiddleware();
+
+    // Endpoint
+    app.MapEndpoint();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseApplication();
-app.UseCustomMiddleware();
-
-// Endpoint
-app.MapEndpoint();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.Information("Application shutting down...");
+    Log.CloseAndFlush();
+}
